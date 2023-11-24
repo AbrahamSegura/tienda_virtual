615eabb5fdea5c2b1d0c2b3df1914d7854851376
Abraham_S <abraham7dsm@gmail.com>
2023-11-23 20:37:40 -0400

    add icons

---
+++ src/components/icons/UserIconDefauld.jsx
@@ -0,0 +1,22 @@
+const UserIconDefauld = (props) => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-2 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      d="M16 15a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v6H6v-6a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3v6h-2v-6Z"
+    />
+  </svg>
+)
+
+export default UserIconDefauld

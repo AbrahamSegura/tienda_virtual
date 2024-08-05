c7a6f4ce6dc7dfe1d6f3fe4929c65e5116952aaf
Abraham_S <abraham7dsm@gmail.com>
2024-08-05 16:41:44 -0400

    icons

---
+++ src/components/icons/ExitIcon.jsx
@@ -0,0 +1,18 @@
+export const ExitIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="#ff6161"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2.5}
+      d="M15 4.001H5v14a2 2 0 0 0 2 2h8m1-5 3-3m0 0-3-3m3 3H9"
+    />
+  </svg>
+)
---
+++ src/components/icons/SeldIcon.jsx
@@ -0,0 +1,14 @@
+export const SeldIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="#0551e7"
+            d="M12.825 22.825 2 12V2h10l10.8 10.85-9.975 9.975ZM6.5 8q.625 0 1.063-.438T8 6.5q0-.625-.438-1.063T6.5 5q-.625 0-1.063.438T5 6.5q0 .625.438 1.063T6.5 8Z"
+        />
+    </svg>
+)
